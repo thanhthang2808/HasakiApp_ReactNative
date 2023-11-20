34bdc0dd0060c2053eb27a204d11afa5d24662b6
thanhthang2808 <thanhthang2808@gmail.com>
2023-11-20 10:24:28 +0700

    Update fetchData

--- fetchData/fetchCategory.js
+++ fetchData/fetchCategory.js
@@ -5,7 +5,7 @@ const fetchCategory = () => {
     useEffect(() => {
         // Mở cmd -> ipconfig
         // Đổi url bên dưới thành địa chỉ ip của máy mình http://192.168.x.x:3000/category trong phần Wifi để có thể truy cập data từ Expo Go
-        fetch('http://192.168.0.4:3000/category')
+        fetch('http://172.16.1.142:3000/category')
         .then(response => response.json())
         .then(data => setCategory(data))
         .catch(error => console.error(error));
--- fetchData/fetchCategoryHomepage.js
+++ fetchData/fetchCategoryHomepage.js
@@ -5,7 +5,7 @@ const fetchCategoryHomepage = () => {
     useEffect(() => {
         // Mở cmd -> ipconfig
         // Đổi url bên dưới thành địa chỉ ip của máy mình http://192.168.x.x:3000/categoryHomepage trong phần Wifi để có thể truy cập data từ Expo Go
-        fetch('http://192.168.0.4:3000/categoryHomepage')
+        fetch('http://172.16.1.142:3000/categoryHomepage')
         .then(response => response.json())
         .then(data => setCateHomepage(data))
         .catch(error => console.error(error));
